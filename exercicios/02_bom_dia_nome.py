a6381bfdf5ca1096bf6af0a65f39df8ace16db89
Marcelle Martins <marcellemartins.96@protonmail.com>
2026-01-29 07:29:31 -0300

    organizar os arquivos em duas pastas. Criararquivos 01, 02, 03, 04 e 05 do dia 2

---
+++ exercicios/02_bom_dia_nome.py
@@ -0,0 +1,5 @@
+#Faça um programa que de bom dia, pergunta o nome da pessoa e responde que é um prazer conhecer ela,
+# citando o nome da pessoa.
+
+nome = input("Bom dia! Qual é o seu nome? ")
+print("Que prazer te conhecer,", nome)
---
+++ exercicios/05_dobro.py
@@ -0,0 +1,7 @@
+# Faça um programa que exiba o dobro de um número inserido pelo usuário.
+
+numero = input("Entre com um valor para calcular o seu dobro: ")
+numero = int(numero)
+
+dobro = numero * 2
+print(numero, "x 2 =", dobro)
