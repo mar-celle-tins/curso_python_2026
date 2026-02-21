9bf6f1d505284bc4adb1bf900dbadce0a31d4228
Marcelle Martins <marcellemartins.96@protonmail.com>
2026-02-20 21:42:56 -0300

    Escrever rascunho de resolução do exercício 1.5 para casa.

--- casa_exercicios/1.5_potencia.py
+++ casa_exercicios/1.5_potencia.py
@@ -1,2 +1,12 @@
 #Faça um programa que receba dois valores A e B. Faça a potência desses dois valores e retorne o resultado:
 # a ^ b = z
+
+#%%
+valor_a = input("Olá! Por favor, escolha um número:")
+valor_a = int(valor_a)
+
+valor_b = input("Agora escolha outro número, por favor:")
+valor_b = int(valor_b)
+
+potencia = valor_a ^ valor_b
+print("A potência dos dois valores escolhidos por você é:", potencia)
