22d25142df11ffd8b6bebb5bbd04852009ed33d0
Marcelle Martins <marcellemartins.96@protonmail.com>
2026-03-12 21:25:37 -0300

    Atualizar documento com exercício 11_alturas_for

--- exercicios_aula/11_alturas_for.py
+++ exercicios_aula/11_alturas_for.py
@@ -1,11 +1,13 @@
 # Faça um programa que receba 4 alturas usando um laço 
 # de repetição e realize a soma dessas alturas.
 
+#%%
 soma = 0 #valor final #variável que vai armazenar todos os valores
 
 qtde_entradas = 4 #contador de entradas (quantidade de "vidas" que eu tenho)
 
-for i in range(qtde_entradas):
+# range(qtde_entradas) = range(0,4) ou range(0, qtde_entradas)
+for i in range(qtde_entradas): 
     altura = input("Entre com a altura: ")
     altura = float(altura)
     soma += altura
